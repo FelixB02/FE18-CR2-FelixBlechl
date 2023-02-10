244278c343673cfea030a8f160ddd85a02fda23b
FelixB02 <felix.blechl@chello.at>
2023-02-10 13:41:32 +0100

    All files

---
+++ Javascript/index.js
@@ -0,0 +1,101 @@
+let array = `
+[{
+    "taskname": "Shopping",
+    "image": "Pictures/shopping.jpg",
+    "description": "Shop for weekly groceries at Billa and Hofer.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Vacuuming",
+    "image": "Pictures/Vacuum-Cleaner.jpg",
+    "description": "Vacuuming every floor in my apartment",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Working out",
+    "image": "Pictures/Workout.jpg",
+    "description": "Going to the gym at least 2 times a week.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Cleaning the windows",
+    "image": "Pictures/Window.jpg",
+    "description": "Cleaning every window in my apartment.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Preparing my Meals",
+    "image": "Pictures/Meal.jpg",
+    "description": "Prepare a good meal 2x a day.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Making the laundry",
+    "image": "Pictures/Laundry.jpg",
+    "description": "Making the laundry 2x a week.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Taking out the trash",
+    "image": "Pictures/Trash.jpg",
+    "description": "Taking out the trash when its full.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Washing the dishes",
+    "image": "Pictures/Dishes.jpg",
+    "description": "Washing the dishes at least once per Day.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}, {
+    "taskname": "Watching a film",
+    "image": "Pictures/Television.jpg",
+    "description": "Watching a film at least once to have some fun.",
+    "deadline": "12.02.2023",
+    "importance": "0"
+}]`
+let final_array = JSON.parse(array);
+
+
+
+
+
+for (let count of final_array) {
+    document.getElementById("result").innerHTML += `
+    <div>
+    <div class="card mb-3" style="width: 24rem;">
+    <button type="button" class="btn btn-info btn-sm" style="width: 8vh; margin: 1vh; color: white;">Task</button>
+    <img class="cardimage" src="${count.image}" class="card-img-top" alt="${count.taskname}">
+    <div class="card-body">
+     <h5 class="card-title text-center">${count.taskname}</h5>
+     <p class="text-center">${count.description}</p>
+     <hr>
+     <p> &#9888;Priority Level:</p>
+     <p class="btn btn-success increase">${count.importance}</p>
+     <p> Deadline: ${ count.deadline}</p>
+     <hr>
+     <p class="btn btn-danger float-end ms-2 ">Delete</p>
+     <p class="btn btn-success float-end ">Done</p>
+    </div>
+    </div>
+    </div>`;
+}
+
+let buttons = document.getElementsByClassName("increase");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        final_array[i].importance++;
+        document.getElementsByClassName("increase")[i].innerHTML = final_array[i].importance;
+        if (final_array[i].importance <= 1) {
+            document.getElementsByClassName("increase")[i].style.backgroundColor = "#008000"
+            document.getElementsByClassName("increase")[i].style.borderColor = "#008000"
+        } else if (final_array[i].importance <= 3) {
+            document.getElementsByClassName("increase")[i].style.backgroundColor = "#a98307"
+            document.getElementsByClassName("increase")[i].style.borderColor = "#a98307"
+        } else if (final_array[i].importance <= 5) {
+            document.getElementsByClassName("increase")[i].style.backgroundColor = "#B22222"
+            document.getElementsByClassName("increase")[i].style.borderColor = "#B22222"
+        }
+    })
+}
